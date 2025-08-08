e472d5689e25171269e5bdba35bfc8d72dc98265
Swann Martin <swannmartin1988@gmail.com>
2025-08-08 21:56:09 +0200

    fix all food and add biome.js

--- lib/getAllFood.ts
+++ lib/getAllFood.ts
@@ -1,27 +1,11 @@
-import { sql } from '@vercel/postgres';
-import { drizzle } from 'drizzle-orm/vercel-postgres';
-import { drizzle as dz } from 'drizzle-orm/node-postgres';
-import { drinks, foods } from '../src/db/schema';
-import 'dotenv/config';
-import { asc, desc, eq } from 'drizzle-orm';
-import { Client } from 'pg';
+import { sql } from "@vercel/postgres";
+import { drizzle } from "drizzle-orm/vercel-postgres";
+import { drinks, foods } from "../src/db/schema";
+import "dotenv/config";
+import { asc } from "drizzle-orm";
 
 export default async function getAllFoods() {
-  const env = process.env?.NODE_ENV;
-  let db = drizzle(sql);
-  if (env === 'development') {
-    const client = new Client({
-      host: process.env.DBHOST,
-      port: !!process.env?.DBPORT ? parseInt(process.env?.DBPORT) : 5432,
-      user: process.env.DBUSER,
-      password: process.env.DBPASSWORD,
-      database: process.env.DBNAME
-    });
-
-    await client.connect();
-
-    db = dz(client);
-  }
+  const db = drizzle(sql);
 
   const dbfood = await db.select().from(foods).orderBy(asc(foods.name));
   const dbdrinks = await db
@@ -31,6 +15,6 @@ export default async function getAllFoods() {
 
   return {
     foods: dbfood,
-    drinks: dbdrinks
+    drinks: dbdrinks,
   };
 }
